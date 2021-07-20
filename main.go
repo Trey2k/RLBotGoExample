c50d44e0e465f838cf4cfce350ceee2118ba5438
Trey M <treym2k.dev@gmail.com>
2021-07-20 19:53:58 -0400

    inital commit

---
+++ main.go
@@ -0,0 +1,61 @@
+// socket client for golang
+// https://golangr.com
+package main
+
+import (
+	"math/rand"
+
+	RLBot "github.com/Trey2k/RLBotGo"
+)
+
+var lastTouch float32
+
+// Tick handler takes in a GameState which contains the gameTickPacket, ballPredidctions, fieldInfo and matchSettings
+func tick(gameState *RLBot.GameState, socket *RLBot.Socket) {
+	// Send a random controller state every time the ball is touched
+	if gameState.GameTick.Ball.LatestTouch.GameSeconds != 0 && gameState.GameTick.Ball.LatestTouch.GameSeconds != lastTouch {
+		PlayerInput := &RLBot.PlayerInput{
+			PlayerIndex: 0,
+			ControllerState: RLBot.ControllerState{
+				Throttle:  rand.Float32(),
+				Steer:     rand.Float32(),
+				Yaw:       rand.Float32(),
+				Pitch:     rand.Float32(),
+				Roll:      rand.Float32(),
+				Jump:      rand.Int31n(1) == 1,
+				Boost:     rand.Int31n(1) == 1,
+				Handbrake: rand.Int31n(1) == 1,
+				UseItem:   rand.Int31n(1) == 1,
+			},
+		}
+
+		lastTouch = gameState.GameTick.Ball.LatestTouch.GameSeconds
+		socket.SendMessage(RLBot.DataType_PlayerInput, PlayerInput)
+	}
+
+}
+
+func main() {
+
+	// connect to RLBot
+	socket, err := RLBot.InitConnection(23234)
+	if err != nil {
+		panic(err)
+	}
+	// Prepare ready message
+	readyMsg := &RLBot.ReadyMessage{
+		WantsBallPredictions: true,
+		WantsQuickChat:       true,
+		WantsGameMessages:    true,
+	}
+
+	// Send ready message
+	err = socket.SendMessage(RLBot.DataType_ReadyMessage, readyMsg)
+	if err != nil {
+		panic(err)
+	}
+
+	// Set our tick handler
+	socket.SetTickHandler(tick)
+
+}
