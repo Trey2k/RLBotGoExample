1e2017201888e9bb6bc1d1c6d61c4b295ee8b52c
Trey M <treym2k.dev@gmail.com>
2021-07-23 19:39:14 -0400

    Updated example

--- main.go
+++ main.go
@@ -1,59 +1,55 @@
 package main
 
 import (
-	"math/rand"
+	"fmt"
 
 	RLBot "github.com/Trey2k/RLBotGo"
 )
 
 var lastTouch float32
+var totalTouches int = 0
 
 // Tick handler takes in a GameState which contains the gameTickPacket, ballPredidctions, fieldInfo and matchSettings
-func tick(gameState *RLBot.GameState, socket *RLBot.Socket) {
+func getInput(gameState *RLBot.GameState, rlBot *RLBot.RLBot) *RLBot.PlayerInput {
 	// Send a random controller state every time the ball is touched
-	if gameState.GameTick.Ball.LatestTouch.GameSeconds != 0 && gameState.GameTick.Ball.LatestTouch.GameSeconds != lastTouch {
-		PlayerInput := &RLBot.PlayerInput{
-			PlayerIndex: 0,
-			ControllerState: RLBot.ControllerState{
-				Throttle:  float32(rand.Int31n(10 - -10)+-10) / 100,
-				Steer:     float32(rand.Int31n(10 - -10)+-10) / 100,
-				Yaw:       float32(rand.Int31n(10 - -10)+-10) / 100,
-				Pitch:     float32(rand.Int31n(10 - -10)+-10) / 100,
-				Roll:      float32(rand.Int31n(10 - -10)+-10) / 100,
-				Jump:      rand.Int31n(1) == 1,
-				Boost:     rand.Int31n(1) == 1,
-				Handbrake: rand.Int31n(1) == 1,
-				UseItem:   rand.Int31n(1) == 1,
-			},
-		}
 
+	PlayerInput := &RLBot.PlayerInput{}
+	PlayerInput.PlayerIndex = 0
+
+	wasjustTouched := false
+	if gameState.GameTick.Ball.LatestTouch.GameSeconds != 0 && lastTouch != gameState.GameTick.Ball.LatestTouch.GameSeconds {
+		totalTouches++
 		lastTouch = gameState.GameTick.Ball.LatestTouch.GameSeconds
-		socket.SendMessage(RLBot.DataType_PlayerInput, PlayerInput)
+		wasjustTouched = true
 	}
+
+	if wasjustTouched && totalTouches <= 10 {
+		rlBot.DebugMessageAdd(fmt.Sprintf("The ball was touched %d times", totalTouches))
+		PlayerInput.ControllerState.Jump = false
+	} else if wasjustTouched && totalTouches > 10 {
+		rlBot.DebugMessageClear()
+		totalTouches = 0
+		PlayerInput.ControllerState.Jump = true
+	}
+	return PlayerInput
 
 }
 
 func main() {
 
 	// connect to RLBot
-	socket, err := RLBot.InitConnection(23234)
+	rlBot, err := RLBot.InitConnection(23234)
 	if err != nil {
 		panic(err)
 	}
-	// Prepare ready message
-	readyMsg := &RLBot.ReadyMessage{
-		WantsBallPredictions: true,
-		WantsQuickChat:       true,
-		WantsGameMessages:    true,
-	}
 
 	// Send ready message
-	err = socket.SendMessage(RLBot.DataType_ReadyMessage, readyMsg)
+	err = rlBot.SendReadyMessage(true, true, true)
 	if err != nil {
 		panic(err)
 	}
 
 	// Set our tick handler
-	socket.SetTickHandler(tick)
+	rlBot.SetGetInput(getInput)
 
 }
